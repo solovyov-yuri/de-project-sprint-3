00a2118f5a62df9eaddcf3bf2cfa182297543f27
Solovyov Yuri <solovevym@gmail.com>
2024-10-15 16:19:23 +0300

    minor

--- migrations/DDL_mart_f_sales.sql
+++ migrations/DDL_mart_f_sales.sql
@@ -1,13 +1,14 @@
-DO $$
-BEGIN
-    IF EXISTS (SELECT 1 FROM information_schema.columns 
-               WHERE table_name='f_sales' 
-               AND table_schema='mart' 
-               AND column_name='status') THEN
-        ALTER TABLE mart.f_sales
-        DROP COLUMN status;
-    END IF;
-END $$;
+-- для тестирования
+-- DO $$
+-- BEGIN
+--     IF EXISTS (SELECT 1 FROM information_schema.columns 
+--                WHERE table_name='f_sales' 
+--                AND table_schema='mart' 
+--                AND column_name='status') THEN
+--         ALTER TABLE mart.f_sales
+--         DROP COLUMN status;
+--     END IF;
+-- END $$;
 
 ALTER TABLE mart.f_sales
 ADD COLUMN status VARCHAR(10) DEFAULT 'shipped' CHECK (status IN ('shipped', 'refunded'));
--- migrations/DDL_staging_user_order_log.sql
+++ migrations/DDL_staging_user_order_log.sql
@@ -1,13 +1,14 @@
-DO $$
-BEGIN
-    IF EXISTS (SELECT 1 FROM information_schema.columns 
-               WHERE table_name='user_order_log' 
-               AND table_schema='staging' 
-               AND column_name='status') THEN
-        ALTER TABLE staging.user_order_log
-        DROP COLUMN status;
-    END IF;
-END $$;
+-- для тестирования
+-- DO $$
+-- BEGIN
+--     IF EXISTS (SELECT 1 FROM information_schema.columns 
+--                WHERE table_name='user_order_log' 
+--                AND table_schema='staging' 
+--                AND column_name='status') THEN
+--         ALTER TABLE staging.user_order_log
+--         DROP COLUMN status;
+--     END IF;
+-- END $$;
 
 ALTER TABLE staging.user_order_log
 ADD COLUMN status VARCHAR(10) DEFAULT 'shipped' CHECK (status IN ('shipped', 'refunded'));
